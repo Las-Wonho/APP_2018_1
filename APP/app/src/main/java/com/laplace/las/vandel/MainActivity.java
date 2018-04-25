693c3dafce47056b126bca3ae881d5e22fcf4812
La <35015234+Las-Wonho@users.noreply.github.com>
2018-04-25 18:56:56 +0900

    기초 작업

---
+++ APP/app/src/main/java/com/laplace/las/vandel/MainActivity.java
@@ -0,0 +1,40 @@
+package com.laplace.las.vandel;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ScrollView scrollView;
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        scrollView = findViewById(R.id.scroll);
+        imageView = findViewById(R.id.controller);
+
+        
+    }
+    public static void scrollToView(View view, final ScrollView scrollView, int count) {
+        if (view != null && view != scrollView) {
+            count += view.getTop();
+            scrollToView((View) view.getParent(), scrollView, count);
+        } else if (scrollView != null) {
+            final int finalCount = count;
+            new Handler().postDelayed(new Runnable() {
+
+                @Override
+                public void run() {
+                    scrollView.smoothScrollTo(0, finalCount);
+                }
+            }, 200);
+        }
+    }
+
+}
