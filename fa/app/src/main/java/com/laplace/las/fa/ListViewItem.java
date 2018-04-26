34aad892428d66d8f784fb86fb17571e15229f86
La <35015234+Las-Wonho@users.noreply.github.com>
2018-04-26 16:14:40 +0900

    tmporory

---
+++ fa/app/src/main/java/com/laplace/las/fa/ListViewItem.java
@@ -0,0 +1,31 @@
+package com.laplace.las.fa;
+
+public class ListViewItem {
+    private int imageID;
+    private String titleStr;
+    private String descStr;
+
+    public int getImageID() {
+        return imageID;
+    }
+
+    public void setImageID(int imageID) {
+        this.imageID = imageID;
+    }
+
+    public String getTitleStr() {
+        return titleStr;
+    }
+
+    public void setTitleStr(String titleStr) {
+        this.titleStr = titleStr;
+    }
+
+    public String getDescStr() {
+        return descStr;
+    }
+
+    public void setDescStr(String descStr) {
+        this.descStr = descStr;
+    }
+}
---
+++ fa/app/src/main/java/com/laplace/las/fa/MainActivity.java
@@ -0,0 +1,34 @@
+package com.laplace.las.fa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("1");
+        list.add("1");
+        list.add("1");
+        list.add("1");
+        list.add("1");
+        list.add("1");
+        list.add("1");
+        list.add("1");
+        list.add("1");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,R.layout.listview_item,list);
+        Spinner spinner = findViewById(R.id.sp);
+        spinner.setAdapter(arrayAdapter);
+
+
+    }
+}
