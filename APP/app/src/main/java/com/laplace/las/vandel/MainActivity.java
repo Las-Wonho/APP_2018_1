c0c315d92278f724176da23043aae55e1de90794
La <35015234+Las-Wonho@users.noreply.github.com>
2018-05-01 15:06:13 +0900

    중간 세이브

--- APP/app/src/main/java/com/laplace/las/vandel/MainActivity.java
+++ APP/app/src/main/java/com/laplace/las/vandel/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.ScrollView;
 
@@ -11,30 +12,15 @@ public class MainActivity extends AppCompatActivity {
 
     ScrollView scrollView;
     ImageView imageView;
+    HorizontalScrollView horizontalScrollView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        scrollView = findViewById(R.id.scroll);
-        imageView = findViewById(R.id.controller);
-
+        imageView = findViewById(R.id.box);
+        scrollView = findViewById(R.id.v);
+        horizontalScrollView = findViewById(R.id.h);
         
-    }
-    public static void scrollToView(View view, final ScrollView scrollView, int count) {
-        if (view != null && view != scrollView) {
-            count += view.getTop();
-            scrollToView((View) view.getParent(), scrollView, count);
-        } else if (scrollView != null) {
-            final int finalCount = count;
-            new Handler().postDelayed(new Runnable() {
-
-                @Override
-                public void run() {
-                    scrollView.smoothScrollTo(0, finalCount);
-                }
-            }, 200);
-        }
     }
 
 }
--- fa/app/src/main/java/com/laplace/las/fa/MainActivity.java
+++ fa/app/src/main/java/com/laplace/las/fa/MainActivity.java
@@ -2,6 +2,8 @@ package com.laplace.las.fa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -28,7 +30,21 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,R.layout.listview_item,list);
         Spinner spinner = findViewById(R.id.sp);
         spinner.setAdapter(arrayAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                list.clear();
+                list.add("qwe2");
+                list.add("qwe3");
+                list.add("qwe5");
 
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
     }
 }
