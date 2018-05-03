95f0858a06bb1716bdebd2792c809419cd33f8ac
La <35015234+Las-Wonho@users.noreply.github.com>
2018-05-03 15:10:50 +0900

    몇가지 바뀜.

---
+++ APP/app/src/main/java/com/laplace/las/vandel/Canvus.java
@@ -0,0 +1,21 @@
+package com.laplace.las.vandel;
+
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Canvus extends AppCompatActivity {
+
+    ConstraintLayout layout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.canvus_layout);
+        layout = findViewById(R.id.canvus_pa);
+
+        layout.setOnTouchListener((v,event)->{
+
+            return true;
+        });
+    }
+}
--- APP/app/src/main/java/com/laplace/las/vandel/MainActivity.java
+++ APP/app/src/main/java/com/laplace/las/vandel/MainActivity.java
@@ -21,28 +21,12 @@ public class MainActivity extends AppCompatActivity {
         scrollView = findViewById(R.id.v);
         horizontalScrollView = findViewById(R.id.h);
 
-        scrollToView(imageView,scrollView,0);
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-
-    }
-    public static void scrollToView(View view, final ScrollView scrollView, int count) {
-        if (view != null && view != scrollView) {
-            count += view.getTop();
-            scrollToView((View) view.getParent(), scrollView, count);
-        } else if (scrollView != null) {
-            final int finalCount = count;
-            new Handler().postDelayed(new Runnable() {
-
-                @Override
-                public void run() {
-                    scrollView.smoothScrollTo(0, finalCount);
-                }
-            }, 200);
-        }
     }
 
 
